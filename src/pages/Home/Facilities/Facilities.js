6d5eaacffa59420d7dc95534f7a9415c26fdf493
sohag-aust <frustratedcoder96@gmail.com>
2022-01-17 22:07:47 +0600

    facility component added for breakdown functionality of facilities component

--- src/pages/Home/Facilities/Facilities.js
+++ src/pages/Home/Facilities/Facilities.js
@@ -1,76 +1,25 @@
-import React, { useEffect } from 'react';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect, useState } from 'react';
 import './Facilities.css';
+import Facility from '../Facility/Facility';
 
 const Facilities = () => {
+    const [facilities, setFacilities] = useState([]);
+
     useEffect(()=>{
-        Aos.init({
-            duration: 2000
-        });
-    }, []);
+        fetch('facilities.json')
+            .then(res=>res.json())
+            .then(data=>setFacilities(data));
+    },[])
 
     return (
         <section className="facility" id="facility">
             <div className="container">
-
                 <h1 className="heading"><span>'</span> our facilities <span>'</span></h1>
 
                 <div className="box-container">
-                    <div className="box" data-aos="zoom-in">
-                        <a href="images/img1.jpg" title="our team">
-                            <img src="images/img1.jpg" alt="" />
-                        </a>
-                    </div>
-
-                    <div className="box" data-aos="zoom-in">
-                        <a href="images/img2.jpg" title="our lab">
-                            <img src="images/img2.jpg" alt="" />
-                        </a>
-                    </div>
-
-                    <div className="box" data-aos="zoom-in">
-                        <a href="images/img3.jpg" title="emergency rooms">
-                            <img src="images/img3.jpg" alt="" />
-                        </a>
-                    </div>
-
-                    <div className="box" data-aos="zoom-in">
-                        <a href="images/img4.jpg" title="expert doctors">
-                            <img src="images/img4.jpg" alt="" />
-                        </a>
-                    </div>
-
-                    <div className="box" data-aos="zoom-in">
-                        <a href="images/img5.jpg" title="expert doctors">
-                            <img src="images/img5.jpg" alt="" />
-                        </a>
-                    </div>
-
-                    <div className="box" data-aos="zoom-in">
-                        <a href="images/img6.jpg" title="emergency rooms">
-                            <img src="images/img6.jpg" alt="" />
-                        </a>
-                    </div>
-
-                    <div className="box" data-aos="zoom-in">
-                        <a href="images/img7.jpg" title="expert doctors">
-                            <img src="images/img7.jpg" alt="" />
-                        </a>
-                    </div>
-
-                    <div className="box" data-aos="zoom-in">
-                        <a href="images/img8.jpg" title="emergency rooms">
-                            <img src="images/img8.jpg" alt="" />
-                        </a>
-                    </div>
-
-                    <div className="box" data-aos="zoom-in">
-                        <a href="images/img9.jpg" title="enough beds">
-                            <img src="images/img9.jpg" alt="" />
-                        </a>
-                    </div>
-
+                    {
+                        facilities?.map(facility => <Facility key={facility.id} facility={facility} />)
+                    }
                 </div>
             </div>
         </section>
---
+++ src/pages/Home/Facility/Facility.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import './Facility.css';
+
+const Facility = (props) => {
+
+    const {title, description, img} = props.facility;
+    // console.log('Facility: ', facility);
+
+    useEffect(()=>{
+        Aos.init({
+            duration: 2000
+        });
+    }, []);
+
+    const cardStyle = {
+        width:'280px',
+        margin:'10px',
+        overflow: 'hidden'
+    }
+
+    return (
+        <>
+            <div className="card text-center" style={cardStyle} data-aos="zoom-in">
+                <img className="card-img" src={img} alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">
+                        <span>{description.substring(0,title.length)}</span>
+                        {description.substring(title.length)}
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <a href="#" className="btn btn-warning">{title}</a>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Facility;
