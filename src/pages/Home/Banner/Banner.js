24fa5ce09417150caa1cba6877ffe818b4ed26d6
sohag-aust <frustratedcoder96@gmail.com>
2022-01-17 12:01:37 +0600

    header and banner added

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import './Banner.css';
+
+const Banner = () => {
+
+    useEffect(()=>{
+        Aos.init({
+            duration: 2000
+        });
+    }, []);
+
+    return (
+        <section className="home" id="home">
+            <div className="container">
+                <div className="row min-vh-100 align-items-center text-center text-md-left">
+
+                    <div className="col-md-6 pr-md-5" data-aos="zoom-in">
+                        <img src="images/home-img.svg" width="100%" alt="" />
+                    </div>
+
+                    <div className="col-md-6 pl-md-5 content" data-aos="fade-left">
+                        <h1><span>stay</span> safe, <span>stay</span> healthy.</h1>
+                        <h3>caring for you.</h3>
+                        <a href="#"><button className="learn-more">learn more</button></a>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        
+    );
+};
+
+export default Banner;
---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <span style={{color:'green'}}>R</span>estore
+                        <span style={{color:'red'}}>H</span>ealth &nbsp;
+                        <span style={{color:'green'}}>B</span>
+                        <span style={{color:'red'}}>D</span>
+                    </Navbar.Brand>
+                    
+                    <Navbar.Toggle />
+
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#facility">Facility</Nav.Link>
+                            <Nav.Link href="#review">Review</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
+                            <Nav.Link href="#post">Post</Nav.Link>
+                            <Nav.Link href="#login">Login</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
